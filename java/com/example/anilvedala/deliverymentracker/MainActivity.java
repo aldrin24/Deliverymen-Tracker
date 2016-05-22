ffbde57414becec79d248eb5069d9fe42b2aee66
anil <anilvedala@gmail.com>
2016-05-22 20:19:59 +0530

    MainActivity readygit status

---
+++ java/com/example/anilvedala/deliverymentracker/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.anilvedala.deliverymentracker;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.content.Intent;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Button shareMyLocation = (Button) findViewById(R.id.share_my_location);
+        Button trackLocation = (Button) findViewById(R.id.location_tracker);
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "Bebas Neue.ttf");
+        shareMyLocation.setTypeface(typeface);
+        trackLocation.setTypeface(typeface);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void showMap(View view){
+        Intent intent = new Intent(this, DisplayMapActivity.class);
+    }
+}
